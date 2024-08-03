3496fcafb780f583cc5bfd07266dd09f9e290dd6
DauntedBocchi <gabrielfamiliasilva10@gmail.com>
2024-08-03 14:14:08 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
 function setup() {
-  createCanvas(1000, 1000);
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background('lightpink');
+  background("lightblue");
+  textSize(150)
+  text('🚗🏡', 100, 320)
+  textSize(100);
+  text('😎', 50, 330);
+  fill("green",);
+  stroke("black");
+  strokeWeight(10);
+    rect(0, 350, 400, 70);  
+  textSize(100);
+  text('✌', 1, 330);
+  textSize(135)
+  text("⛅ 🚁", 1, 130)
 }
